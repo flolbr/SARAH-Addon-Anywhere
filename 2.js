e28313a885bac0dc812ebcb1111fb3f10be661d0
Black3v3r <black3v3r@gmail.com>
2014-02-24 11:56:11 +0100

    New TTS API

--- 2.js
+++ 2.js
@@ -305,11 +305,20 @@
 		});
 
 		function vocalise(text) {
-			console.log('text: ' + text);
-			var audio = new Audio();
-			audio.src ='http://translate.google.com/translate_tts?ie=utf-8&tl=fr&q=' + text;
-			// alert(audio.src);
-			console.log(audio);
-			console.log(audio.src);
-			audio.play();
+			console.log("Bonjour");
+			var msg = new SpeechSynthesisUtterance();
+			var voices = window.speechSynthesis.getVoices();
+			msg.voice = voices[10]; // Note: some voices don't support altering params
+			msg.voiceURI = 'native';
+			msg.volume = 1; // 0 to 1
+			msg.rate = 1; // 0.1 to 10
+			msg.pitch = 2; //0 to 2
+			msg.text = text;
+			console.log(msg.text);
+			msg.lang = 'fr-FR';
+			
+			msg.onend = function(e) {
+			  console.log('Finished in ' + event.elapsedTime + ' seconds.');
+			};
+			speechSynthesis.speak(msg);
 		}
